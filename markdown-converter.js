e3238f3917060615ba910077a0381b8b6b5c79e2
Klinke <klinkwort92@gmail.com>
2019-12-03 21:14:41 +0100

    Created a react class for the converter

--- markdown-converter.js
+++ markdown-converter.js
@@ -1,7 +1,34 @@
 "use strict";
 
-let markdown = <textarea/>;
+
+class MarkdownConverter extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+          text: ""
+        };
+  
+      // This binding is necessary to make `this` work in the callback
+      this.handleChange = this.handleChange.bind(this);
+    }
+  
+    handleChange() {
+      this.setState(state => ({
+        text: "Hello"
+      }));
+    }
+  
+    render() {
+      return (
+          <div>
+          <textarea />
+          
+          </div>
+      );
+    }
+  }
+  
 
 
-ReactDOM.render(markdown, document.getElementById("markdown-converter"));
-ReactDOM.render(previewer, document.getElementById("markdown-previewer"));
+
+ReactDOM.render(<MarkdownConverter />, document.getElementById("markdown-converter"));
