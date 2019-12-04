c97d7d7c77698ceccc2aeb3f35d664747df45265
Klinke <klinkwort92@gmail.com>
2019-12-04 17:17:44 +0100

    Added markdown preview

--- markdown-converter.js
+++ markdown-converter.js
@@ -6,7 +6,8 @@ class MarkdownConverter extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-          text: '**Hello**'
+          text: "# Markdown \nUndertitle \n## Hello \n`<addr>` \n\n ```if (isAwesome){return true}``` \n\n - This to code  \n- Well \n\n\n **As Kanye West said:** \n >We're living the future so \n\n ![Alt Text](/files/maybe.jpg)"
+
         };
   
       this.handleChange = this.handleChange.bind(this);
@@ -15,14 +16,18 @@ class MarkdownConverter extends React.Component {
     handleChange(event) {
         this.setState({
             text: event.target.value
+
           });
     }
       
     render() {
       return (
           <div>
+
           <textarea id="editor" value={this.state.text} onChange={this.handleChange.bind(this)} />
+          
           <div dangerouslySetInnerHTML={{__html: marked(this.state.text)}}></div>
+          
           </div>
       );
     }
